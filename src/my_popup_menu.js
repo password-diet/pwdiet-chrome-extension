75141e58609d3afe37ebaa45743028d29f23883e
unknown <ochronus@ochronus.com>
2015-08-09 19:57:51 +0200

    x

--- src/my_popup_menu.js
+++ src/my_popup_menu.js
@@ -1,3 +1,10 @@
 setInterval(() => {
   console.log("Hi, I am popup")
 }, 1000)
+
+let dwGen=require("diceware-password-generator")
+let copyDiv=document.getElementById("copyme");
+copyDiv.focus();
+document.execCommand("SelectAll");
+document.execCommand("Copy",!1,null);
+console.log(dwGen());
